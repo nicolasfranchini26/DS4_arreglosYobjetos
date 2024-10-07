d18473a416ffa15af431a5ca9bfe58fb05cc5098
isidora franchini <n.franchinic@gmail.com>
2024-10-06 23:56:35 -0300

    Desafío Arreglos y Objetos

---
+++ assets/js/array_propiedades.js
@@ -0,0 +1,151 @@
+export const propiedadesVenta = [
+    {
+        nombre: 'Casa en venta 1',
+        src: 'https://i.pinimg.com/564x/e3/00/61/e30061544451ad0dec123e4a0b16decd.jpg',
+        descripcion: 'Casa en sector residencial',
+        ubicacion: 'Providencia, Santiago-Chile',
+        habitaciones: 3,
+        baños: 3,
+        costo: 100000,
+        animales:false,
+        smoke: true
+        
+    },
+    {
+        nombre: 'Casa en venta 2',
+        src: 'https://i.pinimg.com/564x/d1/ae/64/d1ae64d59c5d1bd8a2934b9f83da0cc8.jpg',
+        descripcion: 'Casa en sector residencial',
+        ubicacion: 'Vitacura, Santiago-Chile',
+        habitaciones:5,
+        baños: 4,
+        costo: 200000,
+        animales: true,
+        smoke: false
+        
+    },
+    {
+        nombre: 'Casa en venta 3',
+        src: 'https://i.pinimg.com/564x/90/f6/d6/90f6d60623d82341a24b0effd9b8514e.jpg',
+        descripcion: 'Casa en sector residencial',
+        ubicacion: 'Chicureo, Santiago-Chile',
+        habitaciones:5,
+        baños: 5,
+        costo: 400000,
+        animales: true,
+        fumar: false
+        
+    },
+    {
+        nombre: 'Casa en venta 4',
+        src: 'https://i.pinimg.com/564x/8d/09/4c/8d094c6896ae7a6f4b0b7022282cf2b3.jpg',
+        descripcion: 'Casa en sector residencial',
+        ubicacion: 'Peñaflor, Santiago-Chile',
+        habitaciones:5,
+        baños: 3,
+        costo: 100000,
+        animales: true,
+        smoke: true
+        
+    },
+    {
+        nombre: 'Casa en venta 5',
+        src: 'https://i.pinimg.com/564x/79/08/39/79083939677d62ca6ad44a45c1fd9d16.jpg',
+        descripcion: 'Casa en sector residencial',
+        ubicacion: 'Viña del Mar, Valparaios-Chile',
+        habitaciones:3,
+        baños: 3,
+        costo: 500000,
+        animales: false,
+        smoke: false
+        
+    },
+    {
+        nombre: 'Casa en venta 6',
+        src: 'https://i.pinimg.com/564x/a2/72/b9/a272b984658aa76c36cc779d17378f23.jpg',
+        descripcion: 'Casa en sector residencial',
+        ubicacion: 'La Reina, Santiago-Chile',
+        habitaciones:4,
+        baños: 3,
+        costo: 900000,
+        animales: false,
+        smoke: false
+        
+    }
+
+];
+
+export const propiedadesAlquiler = [
+    {
+        nombre: 'Casa en Alquier 1',
+        src: 'https://i.pinimg.com/564x/44/4b/d2/444bd242227bb03ba16f5da6a37896bd.jpg',
+        descripcion: 'Casa en sector residencial',
+        ubicacion: 'Calera de Tango, RM-Chile',
+        habitaciones: 5,
+        baños: 4,
+        costo: 900000,
+        animales: false,
+        smoke: true
+        
+    },
+    {
+        nombre: 'Apartamento en Alquiler 2',
+        src: 'https://i.pinimg.com/564x/83/eb/ed/83ebed23d1024f8563748121ce666255.jpg',
+        descripcion: 'Apartamento en sector residencial',
+        ubicacion: 'Santiago, RM-Chile',
+        habitaciones:2,
+        baños: 1,
+        costo: 300000,
+        animales: true,
+        smoke: false
+        
+    },
+    {
+        nombre: 'Apartamento en Alquiler 3',
+        src: 'https://i.pinimg.com/564x/73/50/db/7350dbd722d3c941b9ec37de8012e499.jpg',
+        descripcion: 'Apartamento en sector residencial',
+        ubicacion: 'La Florida, Santiago-Chile',
+        habitaciones: 3,
+        baños: 3,
+        costo: 400000,
+        animales: true,
+        smoke: false
+        
+    },
+    {
+        nombre: 'Casa en Alquier 4',
+        src: 'https://i.pinimg.com/564x/40/88/d6/4088d69b5c6e71257442de0dca8f7eb7.jpg',
+        descripcion: 'Casa en sector residencial',
+        ubicacion: 'Talgante, RM-Chile',
+        habitaciones: 3,
+        baños: 2,
+        costo: 600000,
+        animales: true,
+        smoke: true
+        
+    },
+    {
+        nombre: 'Casa en Alquier 5',
+        src: 'https://i.pinimg.com/564x/eb/70/c8/eb70c8989de7302f8219f3dd049672a4.jpg',
+        descripcion: 'Casa en sector residencial',
+        ubicacion: 'Vitacura, RM-Chile',
+        habitaciones:5,
+        baños: 5,
+        costo: 900000,
+        animales: false,
+        smoke: false
+        
+    },
+    {
+        nombre: 'Apartamento en Alquier 6',
+        src: 'https://i.pinimg.com/564x/bf/7d/41/bf7d41a195fe171c3ca3b5018385c22f.jpg',
+        descripcion: 'Apartamento en sector residencial',
+        ubicacion: 'Las Condes, Santiago-Chile',
+        habitaciones: 3,
+        baños: 2, 
+        costo: 600000,
+        animales: false,
+        smoke: false
+        
+    }
+
+];
---
+++ assets/js/propiedades.js
@@ -0,0 +1,61 @@
+export function propiedades(arryPropiedades, contenedor) {
+    let contenidoHTML = '';
+
+    for (let propiedad of arryPropiedades) {
+
+        
+        let mascotas = (propiedad.smoke?
+          
+            `<p class="text-danger">
+            <i class="fas fa-smoking-ban"></i> No se permite fumar
+        </p>`
+            :
+            `<p class="text-success">
+            <i class="fas fa-smoking"></i> Permitido fumar
+        </p>`)
+
+        
+        let fumar = (propiedad.animales? 
+            
+            `<p class="text-danger">
+            <i class="fas fa-ban"></i> No se permiten mascotas
+            </p>`
+            :
+            `<p class="text-success">
+            <i class="fas fa-paw"></i> Mascotas permitidas
+        </p>`)
+        
+        contenidoHTML += `
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <img
+                        src=${propiedad.src}
+                        class="card-img-top"
+                        alt="Imagen del departamento"
+                    />
+                    <div class="card-body">
+                        <h5 class="card-title">
+                            ${propiedad.nombre}
+                        </h5>
+                        <p class="card-text">
+                            ${propiedad.descripcion}
+                        </p>
+                        <p>
+                            <i class="fas fa-map-marker-alt"></i> ${propiedad.ubicacion}
+                        </p>
+                        <p>
+                            <i class="fas fa-bed"></i> ${propiedad.habitaciones} Habitaciones |
+                            <i class="fas fa-bath"></i> ${propiedad.banos} Baños
+                        </p>
+                        <p><i class="fas fa-dollar-sign"></i> ${propiedad.costo}</p>
+                        ${fumar}
+                        ${mascotas}
+                    </div>
+                </div>
+            </div>
+        `
+    }
+
+    contenedor.innerHTML = contenidoHTML;
+
+}
